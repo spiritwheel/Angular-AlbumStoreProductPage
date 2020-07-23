460750666e62739730d9b31983f204786d48fb6a
spiritwheel <fernandez.logan@yahoo.fr>
2020-07-23 15:36:48 +0200

    3 OK test pass

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-description',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
+  albumInfo ;
+  
+  constructor(private _productService: ProductService) { 
 
-  constructor() { }
+  }
 
   ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
   }
 
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -7,6 +7,6 @@ export class ProductService {
   private _albumUrl: string = '../assets/album.json';
   constructor(private _http: Http) { }
   getAlbum(id: number) {
-    return this._http.get(this._albumUrl);
+    return this._http.get(this._albumUrl).map((response) => response.json());
   }
 }
